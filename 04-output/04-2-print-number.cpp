97a77a90c43c77a13473776066ad1c692e2cdbcc
James Cao <cao.james069@gmail.com>
2025-09-19 08:18:11 +0700

    04 output folder

---
+++ 04-output/04-2-print-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 3 + 3;
+    cout << "\n";
+    cout << 2 * 5;
+    cout << "\n";
+    cout << 3 / 3;
+
+    return 0;
+}
---
+++ 04-output/04-3-new-lines.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "3 + 3 = " << 3 + 3 << "\n";
+    cout << "2 x 5 = " << 2 * 5 << endl;
+
+    return 0;
+}
