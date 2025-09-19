33b4348743961cfe589f69f72a617a521d3fc775
James Cao <cao.james069@gmail.com>
2025-09-19 14:38:33 +0700

    11 math folder

---
+++ 11-math/11-math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    cout << min(x, y) << endl;
+    cout << max (x, y) << endl;
+    cout << sqrt(25);
+
+    return 0;
+}
