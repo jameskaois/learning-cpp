c4ef5050e5dcae3d1f618f765ced0dafabf7d325
James Cao <cao.james069@gmail.com>
2025-10-03 08:21:39 +0700

    23 memory management folder

---
+++ 23-memory-management/23-memory-management.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myNumber = 10;
+    float myFLoat;
+    char myChar;
+    string myString;
+
+    cout << sizeof(myNumber) << endl;
+    cout << sizeof(myFLoat) << endl;
+    cout << sizeof(myChar) << endl;
+    cout << sizeof(myString) << endl;
+
+    return 0;
+}
